1beb7f0ea75e1c91a7311c199b39ef3ac34b78e7
TyJam <djkoatz@gmail.com>
2020-06-28 21:39:05 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,307 @@
+
+const seafood  = `<div id="main-container seafood">
+<div class="info-food Add">
+ <div class="img-holder">
+   <img src="images/fish-food.jpg"  class="bround" alt="">
+   </div>
+   <div class="secbody">
+      <h2>Cajun <span>Salmon</span></h2>
+      <p> Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Totam a saepe voluptatum
+       exercitationem dolores laborum adipisci
+        consequatur, ut eum amet quis tempore 
+        beatae sit hic, quia laboriosam, nam error omnis?</p>
+        <div id="food-half">
+          <span>$22.00</span>
+          <span class="addToshoppingCart"> 
+            Add to Cart 
+          </span>
+        </div>
+   </div>
+   
+ <div class="title dispnone" >
+ <h4 class="food-title">1.Cajun Salmon Alfredo</h4>
+ <div class="Add"> ADD
+
+ </div>
+ <div>$22.00</div>
+</div>
+</div>
+
+<div  class="info-food Add">
+  <div class="img-holder">
+      <img src="images/shrimp.jpg" class="bround" alt="">
+    </div>
+    <div class="secbody">
+      <h2>1/2 Pound <span>Crawfish</span></h2>
+      <p> Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Totam a saepe voluptatum
+       exercitationem dolores laborum adipisci
+        consequatur, ut eum amet quis tempore 
+        beatae sit hic, quia laboriosam, nam error omnis?</p>
+        <div id="food-half">
+          <span>$10.99</span>
+          <span class="addToshoppingCart"> 
+            Add to Cart 
+          </span>
+        </div>
+   </div>
+  <div class="title dispnone" ">
+    <h4 class="food-title">2. 1/2 Pound Crawfish</h4>
+    <div class="Add">ADD</div>
+    <div>$10.99</div>
+  </div>
+</div>
+<div  class="info-food Add">
+<div class="img-holder">
+   <img src="images/GLAZED-SALMON.jpg" class="bround" alt="">
+  </div>
+  <div class="secbody">
+      <h2>Honey Glazed <span>Salmon</span></h2>
+      <p> Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Totam a saepe voluptatum
+       exercitationem dolores laborum adipisci
+        consequatur, ut eum amet quis tempore 
+        beatae sit hic, quia laboriosam, nam error omnis?</p>
+        <div id="food-half">
+          <span>$25.00</span>
+          <span class="addToshoppingCart"> 
+            Add to Cart 
+          </span>
+        </div>
+   </div>
+  <div class="title dispnone">
+  <h4 class="food-title">3.Honey Glazed Salmon</h4>
+  <div class="Add">ADD</div>
+  <div>$25.00</div>
+</div>
+  
+</div>
+</div>`;
+const  Gumbo =`<div id="main-container Gumbo">
+<div  class="info-food Add">
+ <div class="img-holder">
+   <img src="images/Gumbo-11.jpg"  class="bround" alt="">
+ </div>
+   
+ <div class="title dispnone" ">
+ <h4 class="food-title">1.New Orleans Gumbo</h4>
+ <div class="Add">ADD</div>
+ <div>$15.00</div>
+</div>
+</div>
+
+<div  class="info-food Add">
+  <div class="img-holder">
+      <img src="images/pot-of-chicken-sausage-gumbo-600x900.jpg" class="bround" alt="">
+  </div>
+  <div class="title dispnone">
+    <h4 class="food-title">2.Andouille Sauasage Gumbo</h4>
+    <div class="Add">ADD</div>
+    <div>$10:00</div>
+  </div>
+</div>
+<div  class="info-food Add">
+<div class="img-holder">
+   <img src="images/20190905-gumbo-vicky-wasik-4I8A7088-1500x1125.jpg" class="bround" alt="">
+  </div>
+  <div class="title">
+  <h4 class="food-title">3.Sausage and seafood Gumbo</h4>
+  <div class="Add">ADD</div>
+  <div>$15:00</div>
+</div>
+  
+</div>
+</div>`;
+ const Chicken = `<div id="main-container Chicken">
+ <div  class="info-food Add">
+ <div class="img-holder">
+   <img src="images/creamy-cajun-chicken-recipe-1-720x1080.jpg"  class="bround" alt="">
+ </div>
+   
+ <div class="title">
+ <h4 class="food-title">Creamy Chicken</h4>
+ <div class="Add">ADD</div>
+ <div>$11.00</div>
+</div>
+</div>
+
+<div  class="info-food Add">
+  <div class="img-holder">
+      <img src="images/Cajun-Chicken-Drumsticks-1-1.jpg" class="bround" alt="">
+  </div>
+  <div class="title">
+    <h4 class="food-title">2.Chickent Drumsticks</h4>
+    <div class="Add">ADD</div>
+    <div>$8.99</div>
+  </div>
+</div>
+<div  class="info-food Add">
+<div class="img-holder">
+   <img src="images/Baked-Cajun-Chicken-Breasts-7-7.jpg" class="bround" alt="">
+  </div>
+  <div class="title">
+  <h4 class="food-title">3.Baked Chicken Breasts</h4>
+  <div class="Add">ADD</div>
+  <div>$12.99</div>
+</div> 
+</div>
+ </div>`;
+ const Jambalaya = `<div id="main-container Jambalya">
+ <div  class="info-food Add">
+ <div class="img-holder">
+   <img src="images/Easy-Jambalaya-Chicken-Shrimp-and-Andouille.jpg"  class="bround" alt="">
+ </div>
+   
+ <div class="title">
+ <h4 class="food-title">Shimp and Adouille</h4>
+ <div class="Add">ADD</div>
+ <div>$14.99</div>
+</div>
+</div>
+
+<div  class="info-food Add">
+  <div class="img-holder">
+      <img src="images/jambalaya-pork-sausage-2k-56a8c2c95f9b58b7d0f4e1e6.jpg" class="bround" alt="">
+  </div>
+  <div class="title">
+    <h4 class="food-title">Pork sausage Jambalaya</h4>
+    <div class="Add">ADD</div>
+    <div>$13.00</div>
+  </div>
+</div>
+<div  class="info-food Add ">
+<div class="img-holder">
+   <img src="images/Cajun-Jambalaya-Pasta-5.jpg" class="bround" alt="">
+  </div>
+  <div class="title">
+  <h4 class="food-title">1.Jambalaya Pasta</h4>
+  <div class="Add">ADD</div>
+  <div>$12.00</div>
+</div>
+</div>
+ </div>`; 
+ const body = document.querySelector('body')
+ const node = document.getElementById("opt");   
+ const main = document.querySelector('main'); 
+ const div = document.getElementById('#main-container')
+ main.insertAdjacentHTML('afterbegin', seafood)
+ const listofGreeting = [seafood, Gumbo, Chicken, Jambalaya];
+ const btn = document.querySelectorAll('.food');
+ const ADD = document.querySelectorAll(".Add ");
+ const container = document.querySelector("container");
+ const addToshoppingCart = document.querySelectorAll('span.addToshoppingCart');
+   console.log(addToshoppingCart);
+ const removeNode = (e) =>{
+     if(e.target.classList.contains('food')){
+          if(main.hasChildNodes()){
+          main.removeChild(main.childNodes[0]);
+      
+        
+          }
+    
+             
+     }else if (e.target.classList.contains('Add')){
+          container.removeChild(container.childNoes())
+         
+     }
+      
+
+ }
+
+ const addNode = (e) =>{
+  document.addEventListener('click', (e) =>{
+      
+    //Baill if it's not a .nav-link
+    if(!e.target.classList.contains('food')) return;
+      if(e.target.classList.contains('seafood')){
+            removeNode(e);
+            main.insertAdjacentHTML('afterbegin', seafood);
+      }else if(e.target.classList.contains("Gumbo")){
+          removeNode(e);
+          main.insertAdjacentHTML('afterbegin', Gumbo)
+      }else if(e.target.classList.contains('Chicken')){
+         removeNode(e);
+         main.insertAdjacentHTML('afterbegin', Chicken)
+      }else if(e.target.classList.contains("Jambalaya")){
+        removeNode(e);
+        main.insertAdjacentHTML("afterbegin", Jambalaya)
+      }
+
+    //Add the active class 
+    event.target.classList.add('active');
+          
+
+    //Get all nav links 
+    var links = document.querySelectorAll('.food');
+
+     
+    //Loop through each link 
+    for (var i = 0; i< links.length; i++){
+
+      //if the link is the one clicked skip it 
+      if(links[i] === event.target) continue;
+
+      //remove the .active class 
+      links[i].classList.remove('active')
+    }
+
+  }, false);
+ 
+ }
+  
+ const AddTocartPage = async (e) =>{
+  const  add = e.currentTarget;
+  const  addClone = add.cloneNode(true);
+
+  //const {top=0, left=0, width=100, height} = add.getBoundingClientRect();
+  
+  addClone.style.position = 'absolute';
+  addClone.style.top = -10 + 'px';
+  addClone.style.left = 0 + 'px';
+  addClone.style.width =  100+ 'vh';
+  addClone.style.height = 20 + 'vh';
+  addClone.style.fontSize = "0px";
+  addClone.style.display="block";
+  let header = document.querySelector('header');
+  header.style.backgroundColor = "transparent";
+  
+
+  let holder = document.querySelectorAll("div.img-holder");
+    
+  holder.forEach((ele)=>{
+   ele.style.width="360px";
+   ele.style.height="50vh";
+   main.style.height = "0px";
+  });
+  let secbody = document.querySelectorAll("div.secbody")
+   secbody.forEach((div)=>{
+     div.style.display ="block";
+   })
+  
+  let title = document.getElementById("title");
+  title.style.display="none";
+
+  
+  let display = document.querySelectorAll("div.title");
+   display.forEach((el) => {
+      el.style.display="none";
+   });
+  node.style.opacity = '0';
+
+  node.parentNode.appendChild(addClone);
+  
+     
+}
+ const checkOut =() => {
+    console.log("we");
+      
+ }
+ 
+ //Event Listener 
+ADD.forEach(elems => elems.addEventListener("click", AddTocartPage));
+btn.forEach( btnDisplay => btnDisplay.addEventListener('click', addNode ))
+addToshoppingCart.forEach(n => n.addEventListener('click', checkOut) )
+//document.onclick= (e) =>{
+ // console.log(e.target);
+//}
